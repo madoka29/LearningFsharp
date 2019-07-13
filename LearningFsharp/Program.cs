e233f451b8d88efdb9834f58ec447eed88e42e66
madoka29 <tomatoice29@gmail.com>
2019-07-13 15:44:19 +0900

    プロジェクト ファイルを追加します。

---
+++ LearningFsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningFsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
