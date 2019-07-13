ff0001da49806a8615f71b0f946d9c6cc528e40e
madoka29 <tomatoice29@gmail.com>
2019-07-13 16:23:09 +0900

    おみくじ作成。計算アプリのプロジェクト作成

---
+++ CalcApp/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+/// 計算機アプリを作る
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ Omikuji/Program.fs
@@ -0,0 +1,25 @@
+﻿
+// Learn more about F# at http://fsharp.org
+
+/// おみくじを引く
+open System
+
+[<EntryPoint>]
+let main argv =
+    
+    printfn "おみくじを引きますか？ yes => 0 no => 1"
+    let command = Console.ReadLine()
+    
+    match command with
+    | "0" -> printfn "おみくじを引きます..."
+             let omikujiList = ["ハッピー！"; "ちょいハッピー"; "ハッピーなような？"]
+             let rand = new Random();
+             let omikuji = rand.Next(0, 2);
+             let result = omikujiList.[omikuji]
+
+             printfn "あなたの今日の運勢は「%s」です！" result
+             
+    | "1" -> printfn "おみくじを引きません"
+    | _ -> ()
+
+    0 // return an integer exit code
